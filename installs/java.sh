41d93beb03280286219859060a527a1884ec1c2f
Jack Meng <error666jm@gmail.com>
2021-08-25 20:54:53 -0400

    Create java.sh

---
+++ installs/java.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env
+#
+# What is this?
+#
+# This is to install the latest version of Java onto your system.
+# What is Java? Check it out here: https://en.wikipedia.org/wiki/Java_(software_platform)
+# 
+# You can use Java for many different things, such as playing Minecraft, coding your own programs, and more.
+#
+# To run this script, you can either click on it and run it in terminal or go into terminal and run these commands
+# 
+# cd <directory where this file is located> && bash java.sh
+# 
+# If this process works, then you should get a green text saying: "Installing Java Now"
+#
+# For support head over to: https://github.com/Yttrium-Terminus/common_scripts/issues
+#
+
+echo -e "\e[92m> Installing Java Now..." ; echo ;
+
+sudo apt-get update
+
+sudo apt upgrade
+
+echo -e "\e[31m> Up to date" ; echo ;
+
+sudo apt-get install default-jdk
+
+echo -e "\e[34m> Installed Java!" ; echo ;
+
+java --version
