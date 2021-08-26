19dc5f4baada2cd904584a88f8448d4eb7a1a608
Jack Meng <error666jm@gmail.com>
2021-08-26 08:43:11 -0400

    Create wine.sh

---
+++ installs/wine.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env
+
+# Installing Wine
+#
+# Wine is a way for Linux to run Window's compatible applications. 
+# Note: everything here will be most compatible if your device is using Debian 10
+#
+# Run this script:
+#
+# bash wine.sh
+# OR
+# ./wine.sh
+#
+
+echo -e "\e[92m> Installing Wine" ; echo ;
+
+sudo apt update
+echo -e "\e[34m> Updated Package Cache..." ; echo ;
+
+sudo apt -y install gnupg2 software-properties-common
+
+wget -qO - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
+
+echo -e "\e[34m> Fetched GPG key(s)" ; echo ;
+
+sudo apt-add-repository https://dl.winehq.org/wine-builds/debian/
+
+sudo apt update
+
+wget -O- -q https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_10/Release.key | sudo apt-key add -  
+
+echo "deb http://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_10 ./" | sudo tee /etc/apt/sources.list.d/wine-obs.list
+
+echo -e "\e[92m> Fetched the repo" ; echo ;
+
+sudo apt update
+
+echo -e "\e[34m> Type 1 to install the stable version OR type 2 to install the staging version" ; echo ;
+read choice
+
+if [ "$choice" == "1" ] 
+  then
+    sudo apt install --install-recommends winehq-stable
+    
+    wine --version
+    
+    echo -e "\e[92m> Installed Wine Stable" ; echo ;
+    
+  exit
+elif [ "$choice" == "2" ] 
+  then
+    sudo apt install winehq-staging
+    
+    wine --version 
+    
+    echo -e "\e[92m> Installed Wine Staging" ; echo ;
+  exit
+fi
