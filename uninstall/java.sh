3d605bd529329d94f1deeafaad8f958b2039ea89
Jack Meng <error666jm@gmail.com>
2021-08-27 16:40:31 -0400

    Create java.sh

---
+++ uninstall/java.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env
+
+# Remove Java JDK and JRE
+#
+# Run Script:
+#
+# bash java.sh
+# OR
+# ./java.sh
+#
+
+echo -e "\e[92m> This script will not check if Java is installed, proceed? Type 1 for yes OR type 2 for no" ; echo ;
+read choice
+
+if [ "$choice" == "1" ] 
+  then
+  
+  sudo apt remove default-jdk default-jre
+  
+  echo -e "\e[34m> Done." ; echo ;
+  exit
+elif [ "$choice" == "2" ] 
+  then
+  
+  exit
+fi
