07e3b39c71ad98e160f49dcafa9a061c2af11764
Jack Meng <error666jm@gmail.com>
2021-08-27 18:47:17 -0400

    auto yes

--- installs/vscode.sh
+++ installs/vscode.sh
@@ -12,18 +12,18 @@ echo -e "\e[92m> Installing VSCode..." ; echo ;
 
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/ -y
 
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 
 rm -f packages.microsoft.gpg
 
-sudo apt install apt-transport-https
+sudo apt install apt-transport-https -y 
 
 echo -e "\e[31m> Updating Cache..." ; echo ;
 
-sudo apt update
-sudo apt-get upgrade
+sudo apt update -y
+sudo apt-get upgrade -y
 
 echo -e "\e[1m> Type 1 to install regular VSCode or type 2 to install VSCode-Insiders: ";
 read choice;
@@ -31,13 +31,13 @@ read choice;
 if [ "$choice" == "1" ]
   then
     echo -e "\e[34m> Installing VSCode-Main" ; echo;
-    sudo apt install code
+    sudo apt install code -y
     echo -e "\e[92m> Success" ; echo ;
     exit
 elif [ "$choice" == "2" ]
   then
     echo -e "\e[34m> Installing VSCode-Insiders" ; echo;
-    sudo apt install code-insiders
+    sudo apt install code-insiders -y
     echo -e "\e[92m> Success" ; echo ;
     exit
 fi
