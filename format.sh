f44bf7bd1fea51076ac24eae2e87b5eafc70589c
Jack Meng <error666jm@gmail.com>
2021-08-26 20:44:32 -0400

    Create format.sh

---
+++ format.sh
@@ -0,0 +1,15 @@
+# This file is not to be used by any user and is only here to guide me and/or quickly allow me to use certain components of code
+
+# Not ARM
+struct=$(uname -i)
+echo -e "\e[94m> Checking if system is compatible..." ; echo ;
+
+if [[ "$struct" != arm* ]]
+  then
+  
+  exit
+else
+  then
+    echo -e "\e[34m> Device Architecture not supported. For your system's safety, this script has been halted" ; echo ;
+  exit
+fi
