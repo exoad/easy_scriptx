1db8279abdef0f54209643d39d712102d9a62f0b
Jack Meng <error666jm@gmail.com>
2021-08-27 21:10:12 -0400

    upa

--- installs/intellij.sh
+++ installs/intellij.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env
 #
+# NOTICE: THIS IS A VERY BUGGY AND NON-USERFRIENDLY SCRIPT IF YOU ARE NOT USING THE VANILLA SCRIPT. THIS MEANS THIS SCRIPT CAN BECOME VERY 
+# DIFFICULT TO OPERATE IF YOU MODIFY THE WGET AND FOLLOWING. IF YOU HAVE ISSUES, PLEASE LEAVE THEM HERE: https://github.com/Yttrium-Terminus/common_scripts/issues
+#
 # IntelliJ Java IDE Installation
 #
 # This script will download the free Community Edition of IntelliJ Java IDE
 # If you do not have Java installed, this script will automatically install Java JDK 8 for you
 #
 # This script will not be updated constantly with the most current direct download link, so feel free to link your own direct download link 
-# on line 26 and also edit the file name accordingly on line 28
+# on line 26 and also edit the file name accordingly on line 28 along with line 31
 #
 # Run Script:
 #
@@ -28,6 +31,7 @@ if [[ "$struct" != arm* ]]
     #edit this file name if you changed the wget
     tar -xf ideaIC-2021.2.tar.gz
 
+    #edit this file extraction of idea-IC-*
     cd idea-IC-212.4746.92/bin && ./idea.sh
     
     ./idea.sh
